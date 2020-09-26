546a1eb7bfd01976e75fc4b55ddd9927bc140b6c
Maximilian Press <max@phasegenomics.com>
2020-09-26 13:58:14 -0700

    put in GPL license for exact_match.py

--- exact_match.py
+++ exact_match.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 """
 Maximilian Press
+copyright (c) 2020
 
-# this script is 
-# free software under the GNU General Public License (GPL). That 
-# is, you can do whatever you want with it,
-# so long as you don't sell or copyright it.  
-# https://www.gnu.org/licenses/gpl-3.0.en.html
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 """
 import sys
